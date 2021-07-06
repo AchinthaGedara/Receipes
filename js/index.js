ae20d6569feb97a4945ccb88993fd989117ea13c
Achintha <ach82000@gmail.com>
2021-07-06 12:47:52 +1000

    adding loading files from local storage

--- js/index.js
+++ js/index.js
@@ -1,15 +1,8 @@
 // JS file start here
-
-
-
+//Load Current Local storage ID
+let twoDimentionalArrayHTML = [], recipeArray = [];
+let ID;
 // Event Handler for search button
-// Save data to local storage
-divTask.addEventListener("click", (event) => {
-    if (event.target.classList.contains("fa-save"))
-    {
-        console.log("jjj");
-    }
-});
 
 document.querySelector(".search-form-textbox").addEventListener("keydown", (event) => {
 
@@ -22,6 +15,7 @@ document.querySelector(".search-form-textbox").addEventListener("keydown", (even
 
 document.querySelector(".search-button").addEventListener("click", () => {
     validation();
+
 
 });
 
@@ -37,26 +31,31 @@ const validation = () => {
 
 const getReceipes = async (keyWord) => {
     let ingredient = keyWord;
-    let htmlArray = [];    
+    let htmlArray = [], ID = 0;
     let URL = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredient}&app_id=a267c238&app_key=b1f0e8d21ea4382b259a18dcb53006ff`;
     let result = await fetch(URL);
     jsonResult = await result.json();
     console.log(jsonResult.hits);
-    console.log(jsonResult.hits[0]);
+
+
     //generate html
     let jsonResultHits = jsonResult.hits;
     for (let i = 0; i < jsonResultHits.length; i++) {
+
         let list = "";
+        ID++;
         list = generateList(jsonResultHits[i].recipe.ingredients);
-        let html = generateHtml(jsonResultHits[i], list);
+        let html = generateHtml(jsonResultHits[i], list, ID);
         htmlArray.push(html);
+        twoDimentionalArrayHTML.push([ID, jsonResultHits[i].recipe.image, jsonResultHits[i].recipe.label, list, jsonResultHits[i].recipe.url]);
+        console.log(twoDimentionalArrayHTML);
 
     }
     display(htmlArray);
 }
 
 
-const generateHtml = (results, list) => {
+const generateHtml = (results, list, ID) => {
 
     let html = "";
     html +=
@@ -66,29 +65,25 @@ const generateHtml = (results, list) => {
                         <div class="card rounded-3 bg-white shadow-lg  ps-0 card-inner"
                             style="max-width: 640px; max-height:400px;">
                             <div class="row h-100">
-                                <div class="col-sm-4 col-4 d-flex justify-content-start h-100">
-                                    <span class="close-button"><i class="fas fa-window-close fa-lg"></i>
-                                    </span>                                    
-                                        <img src="${results.recipe.image}"
-                                        class="img-fluid rounded-0 card-image" style="object-fit:cover">                                    
+                                <div class="col-sm-4 col-4 d-flex justify-content-start h-100">                                                                        
+                                    <img src="${results.recipe.image}" class="img-fluid rounded-0 card-image" style="object-fit:cover">                                    
                                 </div>
                                 <div class="col-sm-8 col-8  pl-0 py-2">
                                     <div class="card-block">                                                                        
-                                            <h4 >                                          
+                                        <h4>                                          
                                                 ${results.recipe.label}                                            
-                                            </h4>                                                                            
-                                         <hr>     
-                                            <div class="row ingredient-list ps-2">                                                                    
+                                        </h4>                                                                            
+                                        <hr>     
+                                        <div class="row ingredient-list ps-2">                                                                    
                                             <ol class="ingredient-list" >     
-                                                ${list}
+                                                    ${list}
                                             </ol>
                                         </div>
                                         <hr>
-                                        <div class="view-recipe mb-0 pb-0">                                                                                
+                                        <div class="view-recipe mb-0 pb-0" cardID="${ID}">                                                                                
                                             <a href="${results.recipe.url}" target="_blank" class="text-muted pe-4"> View Receipe >> </a>
-                                            <i class="fas fa-save fa-lg"></i>                                         
-                                        </div>
-                                        
+                                            <i class="fas fa-save fa-lg"></i>                                                                           
+                                        </div>                                        
                                     </div>
                                 </div>
                             </div>
@@ -96,14 +91,14 @@ const generateHtml = (results, list) => {
                     </div>
                 </div>
             </div>`;
-    
+
     return html;
 
 }
 
 const display = (htmlArray) => {
     let html = htmlArray.join("\n");
-    
+
     document.querySelector(".recipes").innerHTML = html;
 }
 
--- js/localStorage.js
+++ js/localStorage.js
@@ -1,22 +1,103 @@
 
 
+// Save data to local storage
+divTask.addEventListener("click", (event) => {
+    let cardID;
+    if (event.target.classList.contains("fa-save")) {
+        cardID = event.target.parentElement.getAttribute("cardID");
+        event.target.parentElement.children[1].style.display = 'none';
+        console.log(ID);
+        save(cardID);
 
-const saveData = () => {
-    let recipeID=0;
-    if(recipeID>0)
-    {
-        // Create a JSON string
-     const tasksJson = JSON.stringify(this.tasks);
-     
-     console.log(tasksJson);
-    // Store the JSON string 
+    }
+});
+
+//Load data from local storage upon request
+document.querySelector(".fa-download").addEventListener("click", () => {
+    let [ID, recipeDetails] = load(), htmlArray = [];
+    if (ID > 0) {
+        console.log(recipeDetails);
+        let html = generateHtmlLocalStorage(ID, recipeDetails);
+        document.querySelector(".recipes").innerHTML = html;
+
+    }
+
+});
+
+//Loading values
+const load = () => {
+    let currentId = 0;
+    if (localStorage.getItem("currentID")) {
+        const recipes = localStorage.getItem("tasks");
+        recipeArray = JSON.parse(recipes);
+    }
+    if (localStorage.getItem("currentID")) {
+        currentId = localStorage.getItem("currentID");
+        currentId = Number(currentId);
+    }
+    else {
+        currentId = 0;
+    }
+    return [currentId, recipeArray];
+}
+
+//save method start here
+const save = (newCardID) => {
+    [ID, array] = load();
+    ID++;
+    // Create a JSON string
+    let cardID = Number(newCardID);
+    recipeArray.push([twoDimentionalArrayHTML[cardID - 1][1], twoDimentionalArrayHTML[cardID - 1][2], twoDimentionalArrayHTML[cardID - 1][3], twoDimentionalArrayHTML[cardID - 1][4]]);
+
+    const tasksJson = JSON.stringify(recipeArray);
+
+    console.log(tasksJson);
+    // Store the JSON string /
     localStorage.setItem("tasks", tasksJson);
 
-    // Convert the currentId to a string
-    const currentId = String(this.currentId);
-    console.log(currentId);
-
     // Store the currentId in localStorage
-    localStorage.setItem("currentId", currentId);
-    }
+    localStorage.setItem("currentID", ID);
+
+}
+const generateHtmlLocalStorage = (ID, recipeDetails) => {
+
+    let html = "";
+    recipeDetails.map((param) => {
+        html +=
+            `<div class="row pt-3">
+                <div class="col-12 d-flex justify-content-end">
+                    <div class="card  rounded-3 px-2 py-2 me-5 card-outer" style="max-width: 640px">
+                        <div class="card rounded-3 bg-white shadow-lg  ps-0 card-inner"
+                            style="max-width: 640px; max-height:400px;">
+                            <div class="row h-100">
+                                <div class="col-sm-4 col-4 d-flex justify-content-start h-100">
+                                    <span class="close-button">
+                                    <i class="fas fa-window-close fa-lg"></i>
+                                    </span>                                    
+                                    <img src="${param[0]}" class="img-fluid rounded-0 card-image" style="object-fit:cover">                                    
+                                </div>
+                                <div class="col-sm-8 col-8  pl-0 py-2">
+                                    <div class="card-block">                                                                        
+                                        <h4>                                          
+                                                ${param[1]}                                            
+                                        </h4>                                                                            
+                                        <hr>     
+                                        <div class="row ingredient-list ps-2">                                                                    
+                                            <ol class="ingredient-list" >     
+                                                    ${param[2]}
+                                            </ol>
+                                        </div>
+                                        <hr>
+                                        <div class="view-recipe mb-0 pb-0" cardID="${ID}">                                                                                
+                                            <a href="${param[3]}" target="_blank" class="text-muted pe-4"> View Receipe >> </a>                                                                                                                    
+                                        </div>                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    });
+    return html;
 }
